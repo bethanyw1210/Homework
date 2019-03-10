c01cb8e3411323909063de36a257861920c1a546
Bethany Woodruff <bethany.w1210@gmail.com>
2019-03-09 22:19:49 -0600

    experimenting with switches

---
+++ Assignment 2/Assets/Scripts/PokemonControls.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokemonControls : MonoBehaviour {
+    /*private string pokemon;
+    private int movespeed = 2;
+    private string fire;
+    private string water;
+
+	// Use this for initialization
+	void Start () {
+        pokemonControls();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void pokemonControls(string pokemon)
+    {
+        switch (pokemon)
+        {
+            case (Input.GetKey(KeyCode.Alpha1)):
+                FlareonControl(f);
+                break;
+            case (Input.GetKey(KeyCode.Alpha2)):
+                VaporeonControl();
+                break;
+        }
+    }
+
+    void FlareonControl()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-movespeed,GetComponent<Rigidbody2D>().velocity.y);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movespeed,GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+    void VaporeonControl()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-movespeed,GetComponent<Rigidbody2D>().velocity.y);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movespeed,GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+}
