5d461bb8f72b7cd9e787415ed537ec2a76865d1d
Bethany Woodruff <bethany.w1210@gmail.com>
2019-03-09 21:39:05 -0600

    experimenting with controls

---
+++ Assignment 2/Assets/Scripts/PokemonControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokemonControl : MonoBehaviour {
+    public int movespeed = 1;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.Alpha1))
+        {
+            flareonControl();
+        }
+		
+	}
+
+    void flareonControl()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-movespeed,GetComponent<Rigidbody2D>().velocity.y);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movespeed,GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+}
