aaeea2fa0917a80da234db93a620d43e3b9c5d1e
Bethany Woodruff <bethany.w1210@gmail.com>
2019-03-31 21:32:04 -0600

    Created for each loops

---
+++ Assignments/Assets/Scripts/3/ForeachLoops.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForeachLoops : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string[] type = new string[9];
+
+        type[0] = "Normal: Eevee";
+        type[1] = "Fire: Flareon";
+        type[2] = "Water: Vaporeon";
+        type[3] = "Electric: Jolteon";
+        type[4] = "Psychic: Espeon";
+        type[5] = "Dark: Umbreon";
+        type[6] = "Grass: Leafeon";
+        type[7] = "Ice: Glaceon";
+        type[8] = "Fairy: Sylveon";
+
+        foreach(string item in type)
+        {
+            print(item);
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
