b12ed378b7796c08b45810dff350bb3494a5af33
Bethany Woodruff <bethany.w1210@gmail.com>
2019-03-31 21:32:26 -0600

    Started enums script

---
+++ Assignments/Assets/Scripts/3/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enums : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        enum eevee { Eevee, Flareon, Vaporeon, Jolteon, Espeon, Umbreon, Leafeon, Glaceon, Sylveon }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
