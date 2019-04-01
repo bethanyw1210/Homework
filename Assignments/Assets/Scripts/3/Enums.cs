b7cfb4e83f253eec84654b9d7cf65c6ea52336a7
Bethany Woodruff <bethany.w1210@gmail.com>
2019-03-31 21:33:17 -0600

    added comment to enum script

--- Assignments/Assets/Scripts/3/Enums.cs
+++ Assignments/Assets/Scripts/3/Enums.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enums : MonoBehaviour {
 
-	// Use this for initialization
+	// this is what I found online and how far I got. I don't think we have learned this in class yet? I may be wrong
 	void Start ()
     {
         enum eevee { Eevee, Flareon, Vaporeon, Jolteon, Espeon, Umbreon, Leafeon, Glaceon, Sylveon }
