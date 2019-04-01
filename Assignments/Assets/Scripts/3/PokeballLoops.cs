2fca0a31d7bfdc7f514c870b431fe10df73b62a6
Bethany Woodruff <bethany.w1210@gmail.com>
2019-03-31 21:20:01 -0600

    created loops and while script

---
+++ Assignments/Assets/Scripts/3/PokeballLoops.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokeballLoops : MonoBehaviour {
+
+    public int pokeballs = 20;
+
+	// Use this for initialization
+	void Start () {
+        for(int i=1; i<=pokeballs; i++)
+        {
+            print(pokeballs + " pokeballs in your bag.");
+        }
+
+        for(int i=1; i>pokeballs; i--)
+        {
+            print(pokeballs + " pokeballs in your bag.");
+        }
+
+        while(pokeballs <= 20)
+        {
+            print(pokeballs + " pokeballs in your bag.");
+            pokeballs++;
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
